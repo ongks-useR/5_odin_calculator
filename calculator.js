54c60b761a010957f43652fe689b6e8d8b198551
ongks-useR <45529850+ongks-useR@users.noreply.github.com>
2025-01-11 13:04:48 +0000

    Include event listeners for keyboard. Refer bottom of the JS file.

--- calculator.js
+++ calculator.js
@@ -284,3 +284,132 @@ calculate.addEventListener(
 
     }
 );
+
+
+// Event Listeners for keyboard.
+// Only support numbers 0 to 9; Backspace, Delete, "." and "="
+document.addEventListener(
+    'keydown',
+    e => {
+        
+        const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        if (NUMBERS.includes(Number(e.key))) {
+
+            // Numbers 0 to 9
+            const span = document.createElement('span');
+
+            span.textContent = e.key;
+
+            const operation = document.querySelector(".display.operation");
+            operation.appendChild(span);
+
+            if (between === null) {
+                firstNumber.push(e.key);
+            }
+            else {
+                secondNumber.push(e.key);
+            }
+        }
+        else if (e.key === "Backspace") {
+
+            // Delete the last digit from right
+            const operation = document.querySelector(".display.operation");
+            operation.lastChild.remove();
+    
+            if (secondNumber.length !== 0) {
+                secondNumber.pop()
+            }
+            else if (between !== null) {
+                between = null
+            }
+            else if (firstNumber.length !== 0) {
+                firstNumber.pop()
+            }
+        }
+        else if (e.key === "Delete") {
+
+            // C button equavalent
+            const operation = document.querySelector(".display.operation");
+            operation.textContent = "";
+
+            const result = document.querySelector(".display.result");
+            result.textContent = "";
+
+            firstNumber = [];
+            between = null;
+            secondNumber = [];
+            answer = null;
+        }
+        else if (e.key === ".") {
+
+            // decimal
+            const operation = document.querySelector(".display.operation");
+
+            if (firstNumber.length !== 0 && !firstNumber.includes(e.key)) {
+             
+                const span = document.createElement('span');
+                span.textContent = e.key;
+        
+                operation.appendChild(span);
+                firstNumber.push(e.key);
+            }
+            else if (secondNumber.length !== 0 && !secondNumber.includes(e.key)) {
+                const span = document.createElement('span');
+                span.textContent = e.key;
+        
+                operation.appendChild(span);
+                secondNumber.push(e.key);
+            }
+            else {
+                alert("Not Allowed !!")
+                console.log("Not Allowed !!")
+            }
+        }
+        else if (e.key === "=") {
+
+            // EQUAL
+            const validation = firstNumber.length !== 0 && between !== null && secondNumber.length !== 0
+
+            if (validation) {
+    
+                let firstValue = firstNumber.reduce((total, current) => {
+                    if (current !== "%") {
+                        total = total + current
+                    }
+                    else {
+                        total = Number(total) / 100
+                    }
+    
+                    return total;
+                }, "");
+    
+                let secondValue = secondNumber.reduce((total, current) => {
+                    if (current !== "%") {
+                        total = total + current
+                    }
+                    else {
+                        total = Number(total) / 100
+                    }
+    
+                    return total;
+                }, "");
+        
+                answer = operate(firstValue, between, secondValue);
+        
+                const result = document.querySelector(".display.result");
+                const span = document.createElement('span');
+                span.textContent = answer;
+        
+                result.appendChild(span);
+    
+            }
+            else {
+                alert("Missing inputs !!")
+            }
+        }
+        else {
+            return
+        }
+    }
+)
